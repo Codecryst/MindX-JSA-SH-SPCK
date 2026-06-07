d36c6213ffc1a8b637388d3ce42994f13affdac9
Codecryst <dbhlong@gmail.com>
2026-06-07 15:35:50 +0700

    adding a few small shift

--- statics/scripts/index.js
+++ statics/scripts/index.js
@@ -1,16 +1,5 @@
-function handleCredentialResponse(response) {
-  console.log("Google credential response:", response);
-  alert("Logged in with Google!\nCheck the browser console for the returned token.");
-}
-
-window.onload = function() {
-  google.accounts.id.initialize({
-    client_id: "953663588824-rf70q8gefocahjgp3lj58mr7mtifnc1b.apps.googleusercontent.com",
-    callback: handleCredentialResponse
-  });
-
-  google.accounts.id.renderButton(
-    document.getElementById("googleSignIn"),
-    { theme: "outline", size: "medium", width: 130 }
-  );
-};
+const POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/porygon';
+fetch(POKE_API_URL)
+.then(res => res.json())
+.then((data) => console.log(data))
+.catch(error => console.log(error));
